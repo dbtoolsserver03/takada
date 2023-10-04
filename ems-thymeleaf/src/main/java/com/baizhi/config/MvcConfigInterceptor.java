4b5fa5edc4101358031ac812c9067f8ddc3bc383
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2023-10-04 17:24:27 +0900

    2

--- ems-thymeleaf/src/main/java/com/baizhi/config/MvcConfigInterceptor.java
+++ ems-thymeleaf/src/main/java/com/baizhi/config/MvcConfigInterceptor.java
@@ -7,20 +7,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.baizhi.aop.interceptors.MyInterceptor;
 
 @Configuration
-public class MvcConfigInterceptor  implements WebMvcConfigurer {
+public class MvcConfigInterceptor implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// TODO 自動生成されたメソッド・スタブ
 		WebMvcConfigurer.super.addInterceptors(registry);
-		
+
 		registry.addInterceptor(new MyInterceptor())
-		.addPathPatterns("/hello/**")//添加拦截的请求路径
-		.excludePathPatterns("/hello/world");//添加排除那些请求路径不经过拦截器
+				.addPathPatterns("/**")//添加拦截的请求路径
+				.excludePathPatterns("/login", "user/regist")
+				.excludePathPatterns("/js/**","/css/**","/files/**","/img/**")
+				;//添加排除那些请求路径不经过拦截器
 
-		;
-		
-}
+	}
 
-	
 }
