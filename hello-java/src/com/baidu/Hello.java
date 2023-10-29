41dbd618af191a4dac88f5f250a822f2b15456fb
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2023-10-29 12:36:47 +0900

    调用第三方JAR包

--- hello-java/src/com/baidu/Hello.java
+++ hello-java/src/com/baidu/Hello.java
@@ -1,5 +1,7 @@
 package com.baidu;
 
+import third.my.Tools;
+
 public class Hello {
 
 	// 程序执行入口 main 方法
@@ -10,6 +12,7 @@ public class Hello {
 		// 取字符串数据的第2个字符串
 		System.out.println(args[1]);
 		
+		System.out.println(Tools.funAdd(1,2));
 		// syso + (ctrl+space)/ (alt+/)
 	}
 
