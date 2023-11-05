ee931e10854ccf0609f79d19979eef7d707a6bbc
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2023-11-05 14:20:15 +0900

    11

--- ems-thymeleaf/src/main/java/com/baizhi/config/MvcConfigInterceptor.java
+++ ems-thymeleaf/src/main/java/com/baizhi/config/MvcConfigInterceptor.java
@@ -2,10 +2,7 @@ package com.baizhi.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.baizhi.aop.interceptors.MyInterceptor;
 
 @Configuration
 public class MvcConfigInterceptor implements WebMvcConfigurer {
@@ -13,20 +10,20 @@ public class MvcConfigInterceptor implements WebMvcConfigurer {
 	@Value("${upload.dir}")
 	private String upload;
 	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		// TODO 自動生成されたメソッド・スタブ
-		WebMvcConfigurer.super.addInterceptors(registry);
-
-		registry.addInterceptor(new MyInterceptor())
-				.addPathPatterns("/**")//添加拦截的请求路径
-				.excludePathPatterns("/login", "user/regist")
-				.excludePathPatterns("/js/**","/css/**","/files/**","/img/**"
-						//, "file:"+upload
-						)
-				;//添加排除那些请求路径不经过拦截器
-
-	}
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		// TODO 自動生成されたメソッド・スタブ
+//		WebMvcConfigurer.super.addInterceptors(registry);
+//
+//		registry.addInterceptor(new MyInterceptor())
+//				.addPathPatterns("/**")//添加拦截的请求路径
+//				.excludePathPatterns("/login", "user/regist","user/generateImageCode")
+//				.excludePathPatterns("/js/**","/css/**","/files/**","/img/**"
+//						//, "file:"+upload
+//						)
+//				;//添加排除那些请求路径不经过拦截器
+//
+//	}
 
 //	@Override
 //	public void addResourceHandlers(ResourceHandlerRegistry registry) {
