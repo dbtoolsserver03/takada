7e48ae53ee5fa0e4e800e0558dec06015543d3ab
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2023-11-05 17:54:49 +0900

    将JAVA后台的数据 显示到画面上

--- ems-thymeleaf/src/main/java/com/baizhi/controller/UniqloController.java
+++ ems-thymeleaf/src/main/java/com/baizhi/controller/UniqloController.java
@@ -1,6 +1,7 @@
 package com.baizhi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,13 +18,28 @@ public class UniqloController {
      * @return
      */
     @RequestMapping("searchByCondition")
-    public String lists(@RequestParam String name , @RequestParam String size, @RequestParam String priceBegin, @RequestParam String priceEnd) {
-        log.debug("查询所有员工信息");
+    public String lists(@RequestParam String name ,
+    		@RequestParam String type,
+    		@RequestParam String size,
+    		@RequestParam String priceBegin,
+    		@RequestParam String priceEnd,
+    		Model model) {
+        log.debug("查询所有uniqlo信息");
        
         log.debug(name);
+        log.debug(type);
         log.debug(size);
         log.debug(priceBegin);
         log.debug(priceEnd);
+        
+        // model里的数据，可以显示在画面上。
+        model.addAttribute("namexxx", name);
+        model.addAttribute("priceBegin", priceBegin);
+        
+        
+        // 检索数据库
+     
+        
         return "uniqlo/uniqlolist";
     }
 }
