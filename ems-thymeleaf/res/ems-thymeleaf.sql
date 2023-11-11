fac400b47a1916638b9f1bcaeec9c4399728872c
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2023-11-11 14:02:13 +0900

    sql update hutools

--- ems-thymeleaf/res/ems-thymeleaf.sql
+++ ems-thymeleaf/res/ems-thymeleaf.sql
@@ -9,89 +9,140 @@ IF
 	
 USE `ems-thymeleaf`;
 
+
 -- ----------------------------
 -- Table structure for employee
 -- ----------------------------
 DROP TABLE IF EXISTS `employee`;
-CREATE TABLE `employee` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(60) DEFAULT NULL COMMENT '员工姓名',
-  `salary` double(10,2) DEFAULT NULL COMMENT '员工工资',
-  `birthday` datetime DEFAULT NULL COMMENT '员工生日',
-  `photo` varchar(200) DEFAULT NULL COMMENT '头像路径',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `employee`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `salary` double(10, 2) NULL DEFAULT NULL,
+  `birthday` datetime NULL DEFAULT NULL,
+  `photo` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of employee
+-- ----------------------------
+INSERT INTO `employee` VALUES (1, 'zhang31', 121333.00, '2001-01-01 00:00:00', '20231004145419720.jpeg');
+INSERT INTO `employee` VALUES (2, 'aaa', 111.20, '1991-01-01 00:00:00', '20231011214017155.jpeg');
 
 -- ----------------------------
 -- Table structure for m_code
 -- ----------------------------
 DROP TABLE IF EXISTS `m_code`;
-CREATE TABLE `m_code` (
-  `key` varchar(20) NOT NULL COMMENT 'key',
-  `code` char(2) NOT NULL COMMENT 'code',
-  `value` varchar(50) DEFAULT NULL COMMENT 'value',
-  `delete_flg` char(1) NOT NULL COMMENT '削除フラグ',
-  `create_time` datetime DEFAULT NULL COMMENT '登録日時',
-  `create_user_id` varchar(20) DEFAULT NULL COMMENT '登録者',
-  `update_time` datetime DEFAULT NULL COMMENT '更新日時',
-  `update_user_id` varchar(20) DEFAULT NULL COMMENT '更新者',
-  PRIMARY KEY (`key`,`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='m_code';
+CREATE TABLE `m_code`  (
+  `m_key` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `m_code` char(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `m_value` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `m_order` int NULL DEFAULT NULL,
+  `m_display1` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `m_display2` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `m_display3` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `m_display4` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `m_display5` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `m_display6` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `m_display7` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `m_display8` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `m_display9` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `delete_flg` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `create_user_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
+  `update_user_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`m_key`, `m_code`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'm_code' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of m_code
+-- ----------------------------
+INSERT INTO `m_code` VALUES ('uniqlo_sex', '', ' ', 0, NULL, 'ALL', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL);
+INSERT INTO `m_code` VALUES ('uniqlo_sex', '00', '女', 2, '00:女', '女', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL);
+INSERT INTO `m_code` VALUES ('uniqlo_sex', '01', '男', 1, '01:男', '男', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL);
+INSERT INTO `m_code` VALUES ('uniqlo_size', '', ' ', 0, NULL, 'ALL', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL);
+INSERT INTO `m_code` VALUES ('uniqlo_size', '01', 'S', 1, '01:S', 'S', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL);
+INSERT INTO `m_code` VALUES ('uniqlo_size', '02', 'M', 2, '02:M', 'M', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL);
+INSERT INTO `m_code` VALUES ('uniqlo_size', '03', 'L', 3, '03:L', 'L', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL);
+INSERT INTO `m_code` VALUES ('uniqlo_type', '', ' ', 0, NULL, 'ALL', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL);
+INSERT INTO `m_code` VALUES ('uniqlo_type', '01', '上身', 1, '01:上身', '上身', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL);
+INSERT INTO `m_code` VALUES ('uniqlo_type', '02', '下身', 2, '02:下身', '下身', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL);
+INSERT INTO `m_code` VALUES ('uniqlo_type', '03', '脚部', 3, '03:脚部', '脚部', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0', NULL, NULL, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for t_files
 -- ----------------------------
-DROP TABLE IF EXISTS `uniqlo`;
-CREATE TABLE `uniqlo` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(60) DEFAULT NULL COMMENT '商品名称',
-  `type` char(2) DEFAULT NULL COMMENT '上身/下身/脚',
-  `size` char(2) DEFAULT NULL COMMENT '尺寸规格',
-  `sex` char(2) DEFAULT NULL COMMENT '适用性别',
-  `photo` varchar(200) DEFAULT NULL COMMENT '商品照片路径',
-  `sale_begin` datetime DEFAULT NULL COMMENT '销售开始日',
-  `sale_end` datetime DEFAULT NULL COMMENT '销售终了日',
-  `price` int DEFAULT NULL COMMENT '单价',
-  `lblob` longblob,
-  `delete_flg` char(1) NOT NULL COMMENT '削除フラグ',
-  `create_time` datetime DEFAULT NULL COMMENT '登録日時',
-  `create_user_id` varchar(20) DEFAULT NULL COMMENT '登録者',
-  `update_time` datetime DEFAULT NULL COMMENT '更新日時',
-  `update_user_id` varchar(20) DEFAULT NULL COMMENT '更新者',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='优衣库';
+DROP TABLE IF EXISTS `t_files`;
+CREATE TABLE `t_files`  (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'key',
+  `old_file_name` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `new_file_name` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `ext` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `path` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `size` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `type` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `is_img` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `downcounts` int NULL DEFAULT 0,
+  `username` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `delete_flg` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '0',
+  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_user_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_user_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of t_files
+-- ----------------------------
+INSERT INTO `t_files` VALUES (1, '20210624223336247.jpeg', '20231004180451794a3d25cb4-72e1-475c-b8a5-1e0a2472de08.jpeg', 'jpeg', '2023-10-04', '85209', 'image/jpeg', 'Y', 3, 'admin', '0', '2023-10-04 18:04:51', 'admin', '2023-10-04 18:17:27', 'admin');
+INSERT INTO `t_files` VALUES (2, '20210624223217286.jpeg', '20231009223608229c68d2bf5-25c9-4ea4-ab34-1a317ca2f246.jpeg', 'jpeg', '2023-10-09', '56465', 'image/jpeg', 'Y', 0, 'admin', '0', '2023-10-09 22:36:08', 'admin', '2023-10-09 22:36:08', 'admin');
 
 -- ----------------------------
 -- Table structure for uniqlo
 -- ----------------------------
 DROP TABLE IF EXISTS `uniqlo`;
-CREATE TABLE `uniqlo` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(60) DEFAULT NULL COMMENT '商品名称',
-  `type` char(2) DEFAULT NULL COMMENT '上身/下身/脚',
-  `size` char(2) DEFAULT NULL COMMENT '尺寸规格',
-  `sex` char(2) DEFAULT NULL COMMENT '适用性别',
-  `photo` varchar(200) DEFAULT NULL COMMENT '商品照片路径',
-  `sale_begin` datetime DEFAULT NULL COMMENT '销售开始日',
-  `sale_end` datetime DEFAULT NULL COMMENT '销售终了日',
-  `price` int DEFAULT NULL COMMENT '单价',
-  `delete_flg` char(1) NOT NULL COMMENT '削除フラグ',
-  `create_time` datetime DEFAULT NULL COMMENT '登録日時',
-  `create_user_id` varchar(20) DEFAULT NULL COMMENT '登録者',
-  `update_time` datetime DEFAULT NULL COMMENT '更新日時',
-  `update_user_id` varchar(20) DEFAULT NULL COMMENT '更新者',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='优衣库';
+CREATE TABLE `uniqlo`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `type` char(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '//',
+  `size` char(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `sex` char(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `photo` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `sale_begin` datetime NULL DEFAULT NULL,
+  `sale_end` datetime NULL DEFAULT NULL,
+  `price` int NULL DEFAULT NULL,
+  `lblob` longblob NULL,
+  `delete_flg` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `create_user_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
+  `update_user_id` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 49 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of uniqlo
+-- ----------------------------
+INSERT INTO `uniqlo` VALUES (1, '绵袜子', '01', '03', '01', 'qXKDBhq1jv', '2000-06-17 04:50:02', '2009-09-04 20:24:24', 87, NULL, 'h', '2014-11-19 15:47:15', 'zshen07', '2016-08-13 14:09:59', '2021-10-18');
+INSERT INTO `uniqlo` VALUES (2, '春天衬衫', '02', '02', '00', 'qXKDBhq1jv', '2010-04-17 04:50:02', '2009-09-04 20:24:24', 87, NULL, 'h', '2014-11-19 15:47:15', 'zshen07', '2016-08-13 14:09:59', '2021-10-18');
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(40) DEFAULT NULL COMMENT '用户名',
-  `realname` varchar(60) DEFAULT NULL COMMENT '真实姓名',
-  `password` varchar(40) DEFAULT NULL COMMENT '密码',
-  `gender` tinyint unsigned DEFAULT NULL COMMENT '性别',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `user`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `realname` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `password` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `gender` tinyint UNSIGNED NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'admin', 'tom', '202cb962ac59075b964b07152d234b70', 1);
+INSERT INTO `user` VALUES (2, 'z3', 'tom', '202cb962ac59075b964b07152d234b70', 1);
