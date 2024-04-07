17a65c72e245ba0c9143f613b1c2682b83ff526d
dbtoolsserver03 <72838890+dbtoolsserver03@users.noreply.github.com>
2024-04-07 18:16:36 +0900

    hello 0407

---
+++ hello-java/src/jp/co/google/ssk/HelloWorld.java
@@ -0,0 +1,47 @@
+package jp.co.google.ssk;
+
+/**
+ * JAVA DOC 可识别
+ * 
+ */
+
+
+/*
+  不被JAVA DOC识别的多行注释
+ 
+  
+ */
+
+// 选中要注释掉的代码，ctrl + shift + c
+//aaaa
+//bb
+//ccccc
+
+
+// public class后面的类名就是文件名。
+public class HelloWorld {
+
+	// main 快捷键   alt+/ 或 ctrl+space 自动联想
+	
+	// 程序入口方法
+	
+	// 方法（返回值，方法名，参数{参数类型，参数个数，参数顺序}）
+	public static void main(String[] args) {
+
+		// syso  快捷键   alt+/  或 ctrl+space 自动联想
+		System.out.println("hello world 12345  !! ");
+		System.err.println(2133);
+
+		int a = 3;
+		int b = 5;
+		System.out.println(a + b);
+
+		// 快捷键  ctrl + shift + f 美化代码
+	}
+
+}
+
+ class Goodmorning {
+
+
+}
