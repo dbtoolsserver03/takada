c9d1555abdf07b7eaf7720cbff01aa1df870350c
dbtoolsserver03 <72838890+dbtoolsserver03@users.noreply.github.com>
2024-04-14 15:10:37 +0900

    java 2

--- hello-java/src/jp/co/google/ssk/basic/TestFunc.java
+++ hello-java/src/jp/co/google/ssk/basic/TestFunc.java
@@ -12,9 +12,21 @@ public class TestFunc {
 
 		int sum = add(a, b);
 
+		long aa = 22;
+		long bb = 33;
+		
+		long sumsum = add(aa,bb);
+		
 		System.out.println(sum);
 		System.out.println(a);
+		System.out.println(sumsum);
 
+	}
+
+	// 方法重载（方法名相同，但是参数类型，参数个数，参数顺序不一样）
+	private static long add(long aa, long bb) {
+		
+		return aa+bb;
 	}
 
 	private static int add(int aa, int bb) {
