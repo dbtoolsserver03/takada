7ce2d2e2471e4255e3bb245da32fbe11148aabf7
dbtoolsserver03 <72838890+dbtoolsserver03@users.noreply.github.com>
2024-04-14 10:59:46 +0900

    0414

--- hello-java/src/jp/co/google/ssk/HelloWorld.java
+++ hello-java/src/jp/co/google/ssk/HelloWorld.java
@@ -5,7 +5,6 @@ package jp.co.google.ssk;
  * 
  */
 
-
 /*
   不被JAVA DOC识别的多行注释
  
@@ -17,14 +16,16 @@ package jp.co.google.ssk;
 //bb
 //ccccc
 
+//类的首字母一定要大写
+//方法的首这母一定要小写
 
 // public class后面的类名就是文件名。
 public class HelloWorld {
 
 	// main 快捷键   alt+/ 或 ctrl+space 自动联想
-	
+
 	// 程序入口方法
-	
+
 	// 方法（返回值，方法名，参数{参数类型，参数个数，参数顺序}）
 	public static void main(String[] args) {
 
@@ -36,12 +37,15 @@ public class HelloWorld {
 		int b = 5;
 		System.out.println(a + b);
 
+		if (args != null && args.length > 0) {
+			System.out.println("output args:" + args[0]);
+		}
+
 		// 快捷键  ctrl + shift + f 美化代码
 	}
 
 }
 
- class Goodmorning {
-
+class Goodmorning {
 
 }
