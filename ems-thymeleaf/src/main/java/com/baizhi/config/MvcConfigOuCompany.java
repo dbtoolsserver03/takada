2e635b7fd942575b461597570df120b56ff3b7fc
Lenovo <Lenovo@192.168.1.40>
2024-05-12 11:39:11 +0900

    Merge remote-tracking branch 'origin/202404_master' into 202404_kiki

---
+++ ems-thymeleaf/src/main/java/com/baizhi/config/MvcConfigOuCompany.java
@@ -0,0 +1,28 @@
+package com.baizhi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfigOuCompany  implements WebMvcConfigurer {
+
+    //通过这里面配置: 不需要为每一个访问thymeleaf模板页面单独开发一个controller请求了
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        //viewController 请求路径    viewName: 跳转视图
+    	// 首页跳转
+    	registry.addViewController("/").setViewName("redirect:/login");
+    	
+    	// 菜单画面
+    	registry.addViewController("/menu").setViewName("menu");
+
+    	// 画面登陆
+        registry.addViewController("login").setViewName("login");
+        
+        // 新用户注册
+        registry.addViewController("register").setViewName("user/regist");
+        
+    }
+}
