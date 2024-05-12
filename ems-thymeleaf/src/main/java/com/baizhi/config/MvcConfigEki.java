696227afe8100bbb8e7fdd4b9d40883e8622c984
24989 <24989@DESKTOP-HU9B3QS.mshome.net>
2024-05-12 11:30:59 +0900

    Merge remote-tracking branch 'origin/202404_master' into 202404_SYY

---
+++ ems-thymeleaf/src/main/java/com/baizhi/config/MvcConfigEki.java
@@ -0,0 +1,21 @@
+package com.baizhi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfigEki  implements WebMvcConfigurer {
+
+    //通过这里面配置: 不需要为每一个访问thymeleaf模板页面单独开发一个controller请求了
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        //viewController 请求路径    viewName: 跳转视图
+    	registry.addViewController("/manageMetro").setViewName("/Metro/Test");
+    	
+        // 增加员工
+        //registry.addViewController("student/addStudent").setViewName("stu/addStudent");
+
+    }
+}
--- hello-java/src/jp/co/google/sun/Sun.java
+++ hello-java/src/jp/co/google/sun/Sun.java
@@ -4,7 +4,8 @@ public class Sun {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("");
 	}
 
 }
+
