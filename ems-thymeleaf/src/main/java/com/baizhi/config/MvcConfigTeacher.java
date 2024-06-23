203a98afce2752e54de5c7f764f438fbc28945d9
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2024-06-23 16:43:24 +0900

    01.修改menu.html文件 02.使用WebMvcConfigurer，不经过Control 直接跳转到HTML

---
+++ ems-thymeleaf/src/main/java/com/baizhi/config/MvcConfigTeacher.java
@@ -0,0 +1,17 @@
+package com.baizhi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfigTeacher  implements WebMvcConfigurer {
+
+    //通过这里面配置: 不需要为每一个访问thymeleaf模板页面单独开发一个controller请求了
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        // 接收请求后，直接返回到画面
+        registry.addViewController("manageTeacher").setViewName("teacher/testTeacher");
+    }
+}
