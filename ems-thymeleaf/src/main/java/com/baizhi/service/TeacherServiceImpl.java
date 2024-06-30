48fc76493b8053c77006d6115fe1bfcf0034487c
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2024-06-30 16:43:41 +0900

    04。真一览作成
        1。Service作成
    
           接口：
           /ems-thymeleaf/src/main/java/com/baizhi/service/TeacherService.java
    
           实现类：
           通过自动生成的mapper来调SQL文，操作数据库
           /ems-thymeleaf/src/main/java/com/baizhi/service/TeacherServiceImpl.java
    
        2。控制器里追加上面的Service成员
           通过Service来取得真实数据
           /ems-thymeleaf/src/main/java/com/baizhi/controller/TeacherController.java

---
+++ ems-thymeleaf/src/main/java/com/baizhi/service/TeacherServiceImpl.java
@@ -0,0 +1,47 @@
+package com.baizhi.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.baizhi.dao.original.TTeacherMapper;
+import com.baizhi.entity.original.TTeacher;
+
+@Service
+@Transactional
+public class TeacherServiceImpl implements TeacherService {
+
+    @Autowired
+    TTeacherMapper mapper;
+
+    @Override
+    public List<TTeacher> lists() {
+        return mapper.selectByExample(null);
+    }
+
+//
+//    @Override
+//    public void save(TStudent obj) {
+//        mapper.insertSelective(obj);
+//
+//    }
+//
+//    @Override
+//    public TStudent findRecByPk(int id) {
+//        return mapper.selectByPrimaryKey(id);
+//
+//    }
+//
+//    @Override
+//    public void update(TStudent obj) {
+//        mapper.updateByPrimaryKeySelective(obj);
+//    }
+//
+//    @Override
+//    public void delete(Integer id) {
+//        mapper.deleteByPrimaryKey(id);
+//    }
+
+}
