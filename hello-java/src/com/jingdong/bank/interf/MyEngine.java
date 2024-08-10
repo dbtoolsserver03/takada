90a889e1bd8005975a8dc052c9412ce757714611
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2024-08-10 14:41:15 +0900

    ゴミソース削除

--- hello-java/src/com/jingdong/bank/interf/MyEngine.java
+++
@@ -1,6 +0,0 @@
-package com.jingdong.bank.interf;
-
-public interface MyEngine {
-
-    public void callMyEngine();
-}
--- hello-java/src/jp/co/yahoo/study/zhang3/d0820/cls/basic/TBasic.java
+++
@@ -1,9 +0,0 @@
-package jp.co.yahoo.study.zhang3.d0820.cls.basic;
-
-public class TBasic {
-
-	public static void main(String[] args) {
-		
-		//Person obj = new Person();
-	};
-}
