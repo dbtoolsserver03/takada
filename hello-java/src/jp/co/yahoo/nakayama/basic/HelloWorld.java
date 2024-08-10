089a8bdcb1f989cbe1648d30fd3f503ebeda0c9b
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2024-08-10 14:41:29 +0900

    java 01

---
+++ hello-java/src/jp/co/yahoo/nakayama/basic/HelloWorld.java
@@ -0,0 +1,35 @@
+// 当ファイルの保存フォルダー
+package jp.co.yahoo.nakayama.basic;
+
+/*
+	public     誰でも参照できるクラス
+	class      このファイルはクラスです
+	HelloWorld クラス名です
+*/
+public class HelloWorld {
+
+//	main : ctrl + space
+
+/*
+	public         誰でも参照できるメソッド
+	static         全局メソッド（JVMメモリの全局）
+	void           戻り値はない意味
+	main           特別なメソッド名です。※applicationの入口
+	String[] args  メソッドのパラメーター
+*/
+	public static void main(String[] args) {
+		
+		// syso : ctrl + space
+		
+		// consoleに指定文字列を出力する
+		System.out.println("hello world 2");
+	}
+	
+/*
+	人
+		HelloWorld.java はプログラム修正のところ
+	
+	PC
+	    HelloWorld.classは二進法  JVM実行ファイル
+*/  
+}
