9d73c7b29827505da0af939e688cb0b3d8a17868
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2024-08-17 15:16:50 +0900

    １

--- hello-java/src/jp/co/yahoo/nakayama/basic/Basic09Pj.java
+++ hello-java/src/jp/co/yahoo/nakayama/basic/Basic09Pj.java
@@ -1,11 +1,9 @@
 package jp.co.yahoo.nakayama.basic;
 
-import calc.CalcTest;
-
 public class Basic09Pj {
 
 	public static void main(String[] args) {
-		System.out.println(CalcTest.multi(2, 3));
+//		System.out.println(CalcTest.multi(2, 3));
 	}
 
 }
