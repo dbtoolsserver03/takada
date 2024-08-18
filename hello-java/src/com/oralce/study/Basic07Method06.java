1365a565bf0e95035daf7fe03cea034ccb53a5e0
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2024-08-18 11:40:45 +0900

    java method

---
+++ hello-java/src/com/oralce/study/Basic07Method06.java
@@ -0,0 +1,14 @@
+package com.oralce.study;
+
+public class Basic07Method06 {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		
+		// 通过工程参照或JAR文件参照可以调用其他工程的方法
+//		int c  = Test3Plugin.minis(a,b);
+//		System.out.println(c);
+	}
+}
