25e3fdbf6c7421480e02734c859667d1c6135552
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2024-08-25 18:00:31 +0900

    Merge branch '202408_z3' of git@github.com:dbtoolsserver03/takada.git into 202408_z3

--- hello-java/src/cn/itcast/mybatis/jdbc/JdbcTest1.java
+++ hello-java/src/cn/itcast/mybatis/jdbc/JdbcTest1.java
@@ -18,21 +18,25 @@ public class JdbcTest1 {
 		ResultSet resultSet = null;
 
 		try {
+
+			
 			//加载数据库驱动
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			//通过驱动管理类获取数据库链接
-			connection =  DriverManager.getConnection("jdbc:mysql://localhost:3306/livan?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true", "root", "123456");
+			connection =  DriverManager.getConnection("jdbc:mysql://localhost:3306/testz3?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true", "root", "123456");
 			//定义sql语句 ?表示占位符
-			String sql = "select student_id,course_id cid from score_table where student_id = 's001'";
+			String sql = "select  name , salary sal from   TEACHER  where   name like '%a%'";
 			//获取预处理statement
 			preparedStatement = connection.prepareStatement(sql);
 			//向数据库发出sql执行查询，查询出结果集
 			resultSet =  preparedStatement.executeQuery();
 			//遍历查询结果集
 			while(resultSet.next()){
-				System.out.println(resultSet.getString("student_id")+"  "+resultSet.getString("cid"));
+				System.out.println(resultSet.getString("name")+"  "+resultSet.getString("sal"));
 			}
+			
+			System.out.println(" finish ");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally{
--- hello-java/src/cn/itcast/mybatis/jdbc/JdbcTestUpt.java
+++ hello-java/src/cn/itcast/mybatis/jdbc/JdbcTestUpt.java
@@ -20,36 +20,34 @@ public class JdbcTestUpt {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			//通过驱动管理类获取数据库链接
-			connection =  DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true", "root", "123456");
+			connection =  DriverManager.getConnection("jdbc:mysql://localhost:3306/testz3?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true", "root", "123456");
 			
+			// 进行事务处理是
 			connection.setAutoCommit(false);
 			
-			//定义sql语句 ?表示占位符
-			String sql1 = "update t_emp set sal = sal- ? where first_nm = ?";
+			//定义sql语句 
+			String sql1 = "update teacher set salary = salary-100  where name = 'zhang3'";
 			//获取预处理statement
 			preparedStatement = connection.prepareStatement(sql1);
-			//设置参数，第一个参数为sql语句中参数的序号（从1开始），第二个参数为设置的参数值
-			preparedStatement.setInt(1, 10);
-			preparedStatement.setString(2, "中島");
 			
 			//向数据库发出sql执行
 			int cnt1 =  preparedStatement.executeUpdate();
 			System.out.println(cnt1 + "更新済み");
 			
-			//定义sql语句 ?表示占位符
-			String sql2 = "update t_emp set sal = sal+ ? where first_nm = ?";
+			//定义sql语句 
+			String sql2 = "update teacher set salary = salary+100  where name = 'li4'";
 			//获取预处理statement
 			preparedStatement = connection.prepareStatement(sql2);
-			//设置参数，第一个参数为sql语句中参数的序号（从1开始），第二个参数为设置的参数值
-			preparedStatement.setInt(1, 10);
-			preparedStatement.setString(2, "木村");
 			
 			//向数据库发出sql执行
 			int cnt2 =  preparedStatement.executeUpdate();
 			
-			connection.commit();
 			System.out.println(cnt2 + "更新済み");
+
 			
+			connection.commit();
+			System.out.println("提交事物，反映到SSD/HHD");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			try {
