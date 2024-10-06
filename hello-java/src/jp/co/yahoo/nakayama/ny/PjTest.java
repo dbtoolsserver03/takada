3611002b53a958541b686b24d85f71451dc6921d
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2024-10-06 13:26:13 +0900

    error

--- hello-java/src/jp/co/yahoo/nakayama/ny/PjTest.java
+++ hello-java/src/jp/co/yahoo/nakayama/ny/PjTest.java
@@ -1,16 +1,16 @@
-package jp.co.yahoo.nakayama.ny;
-
-import calc.Abc;
-import jp.co.Aminusb;
-
-public class PjTest {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-      //
-      System.out.println(Abc.multi(9, 13));
-      System.out.println(Aminusb.minus(11, 5));
-	}
-	
-
-}
+//package jp.co.yahoo.nakayama.ny;
+//
+//import calc.Abc;
+//import jp.co.Aminusb;
+//
+//public class PjTest {
+//
+//	public static void main(String[] args) {
+//		// TODO 自動生成されたメソッド・スタブ
+//      //
+//      System.out.println(Abc.multi(9, 13));
+//      System.out.println(Aminusb.minus(11, 5));
+//	}
+//	
+//
+//}
